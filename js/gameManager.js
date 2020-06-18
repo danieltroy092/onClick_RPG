f0a57fca0bf8a914c86ea0726339be091cc14a86
Daniel Troy Ong <danieltroy092@gmail.com>
2020-06-18 16:21:18 +0100

    Implemented setFight function within GameManager

--- js/gameManager.js
+++ js/gameManager.js
@@ -4,7 +4,7 @@ let GameManager = {
     this.setPreFight(classType);
   },
   resetPlayer: function (classType) {
-    // starting player stats
+    // Create Player
     switch (classType) {
       case 'Warrior': // Class Type
         player = new Player(classType, 200, 0, 200, 100, 50); //  set stats
@@ -19,6 +19,8 @@ let GameManager = {
         player = new Player(classType, 200, 0, 50, 200, 100); //  set stats
         break;
     }
+
+    // Manipulate the DOM to set values for player attributes per classType
     let getInterface = document.querySelector('.interface');
     getInterface.innerHTML =
       '<img src="img/player/' +
@@ -37,5 +39,60 @@ let GameManager = {
       player.speed +
       '</p></div>';
   },
-  setPreFight: function () {},
+  // Set Environment
+  setPreFight: function () {
+    let getHeader = document.querySelector('.header');
+    let getActions = document.querySelector('.actions');
+    let getArena = document.querySelector('.arena');
+    getHeader.innerHTML = '<p>Quest: Locate Enemy!</p>';
+    getActions.innerHTML =
+      '<a href="#" class="btn-prefight" onClick="GameManager.setFight()">Locate Enemy!</a>';
+    getArena.style.visibility = 'visible';
+  },
+
+  // Set Fight
+  setFight: function () {
+    let getHeader = document.querySelector('.header');
+    let getActions = document.querySelector('.actions');
+    let getEnemy = document.querySelector('.enemy');
+
+    // Create enemy
+    let enemy00 = new Enemy('Goblin', 100, 0, 50, 200, 200); // Class & Stats
+    let enemy01 = new Enemy('Orc', 150, 0, 100, 50, 50); // Class & Stats
+    let enemy02 = new Enemy('Beast', 200, 0, 150, 100, 100); // Class & Stats
+
+    // Randomise Enemy
+    let pickRandomEnemy = Math.floor(Math.random() * Math.floor(3));
+    switch (pickRandomEnemy) {
+      case 0:
+        enemy = enemy00; // Goblin
+        break;
+      case 1:
+        enemy = enemy01; // Orc
+        break;
+      case 2:
+        enemy = enemy02; // Beast
+        break;
+    }
+    // Manipulate the DOM to set values for enemy attributes per enemyType
+    getHeader.innerHTML = '<p>Task: Choose your move!</p>';
+    getActions.innerHTML =
+      '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>';
+    getEnemy.innerHTML =
+      '<img src="img/enemy/' +
+      enemy.enemyType.toLowerCase() +
+      '.png" class="img-avatar"><div><h3>' +
+      enemy.enemyType +
+      '</h3><p>Health: ' +
+      enemy.health +
+      '</p><p>Mana: ' +
+      enemy.mana +
+      '</p><p>Strength: ' +
+      enemy.strength +
+      '</p><p>Agility: ' +
+      enemy.agility +
+      '</p><p>Speed: ' +
+      enemy.speed +
+      '</p></div>';
+  },
 };
